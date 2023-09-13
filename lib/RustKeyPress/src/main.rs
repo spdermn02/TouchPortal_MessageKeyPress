fce222e01c2be8e9e64bebd22ed73c37f25cccc3
Jameson Allen <jameson.f.allen@gmail.com>
2023-09-13 10:58:02 -0500

    feat(rust): [TP-1] Adding in Rust Source

---
+++ lib/RustKeyPress/src/main.rs
@@ -0,0 +1,9 @@
+use enigo::*;
+use std::env::args;
+
+fn main() {
+    let mut enigo = Enigo::new();
+    let message = args().nth(1).unwrap();
+    // write text
+    enigo.key_sequence(message.as_str());
+}
